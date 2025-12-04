c64f9c46cf4ee7c4d7c3cebdb2d8fb2231acd9db
MarcoVacchi <mvacchi816@gmail.com>
2025-12-04 15:11:49 +0100

    Refactor: adjust indentation in VehicleVariationDTO for improved readability

--- src/main/java/org/lessons/vehicles/java/vehicleVariation/dto/VehicleVariationDTO.java
+++ src/main/java/org/lessons/vehicles/java/vehicleVariation/dto/VehicleVariationDTO.java
@@ -3,9 +3,9 @@ package org.lessons.vehicles.java.vehicleVariation.dto;
 import java.sql.Date;
 
 public record VehicleVariationDTO(
-                Integer cc,
-                Date immatricolationMonth,
-                Integer immatricolationYear,
-                String fuelSystemIt,
-                String fuelSystemEn) {
+        Integer cc,
+        Date immatricolationMonth,
+        Integer immatricolationYear,
+        String fuelSystemIt,
+        String fuelSystemEn) {
 }
